3b603a67cf160db6e1c8a4556355bc43f7ed63a9
Anton Borodaenko <k4p1toxa@gmail.com>
2020-08-09 18:47:38 +0300

    Add bottom navigation view

--- app/src/main/java/ru/kapitoxa/mywallet/MainActivity.kt
+++ app/src/main/java/ru/kapitoxa/mywallet/MainActivity.kt
@@ -2,32 +2,42 @@ package ru.kapitoxa.mywallet
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import ru.kapitoxa.mywallet.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        navController = this.findNavController(R.id.nav_host_fragment)
-
-        NavigationUI.setupActionBarWithNavController(this, navController)
-
-        navController.addOnDestinationChangedListener { _, destination, _ ->
-            if (destination.id == R.id.operationDetailFragment
-                    || destination.id == R.id.categoryDetailFragment) {
-                supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_baseline_close_24)
-            } else {
-                supportActionBar?.setIcon(R.drawable.ic_baseline_arrow_back_24)
-            }
-        }
+        setupNavigation()
     }
 
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+    private fun setupNavigation() {
+        navController = this.findNavController(R.id.nav_host_fragment)
+
+        NavigationUI.setupActionBarWithNavController(this, navController)
+        binding.bottomNavView.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            val toolbar = supportActionBar ?: return@addOnDestinationChangedListener
+            if (destination.id == R.id.operationDetailFragment
+                    || destination.id == R.id.categoryDetailFragment) {
+                toolbar.setHomeAsUpIndicator(R.drawable.ic_baseline_close_24)
+            } else {
+                toolbar.setIcon(R.drawable.ic_baseline_arrow_back_24)
+            }
+        }
     }
 }
