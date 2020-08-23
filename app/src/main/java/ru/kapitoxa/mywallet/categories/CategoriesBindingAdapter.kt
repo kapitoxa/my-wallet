4d90b24170199afe195458732d3ac25dbed5163c
Anton Borodaenko <k4p1toxa@gmail.com>
2020-08-23 11:35:13 +0300

    Extract icon colors to resources

--- app/src/main/java/ru/kapitoxa/mywallet/categories/CategoriesBindingAdapter.kt
+++ app/src/main/java/ru/kapitoxa/mywallet/categories/CategoriesBindingAdapter.kt
@@ -6,7 +6,7 @@ import androidx.databinding.BindingAdapter
 import ru.kapitoxa.mywallet.R
 import ru.kapitoxa.mywallet.database.CategoryWithType
 import java.util.*
-import kotlin.random.Random
+import kotlin.math.abs
 
 @BindingAdapter("categoryTypeColor")
 fun TextView.bindCategoryTypeColor(item: CategoryWithType?) {
@@ -30,43 +30,14 @@ fun TextView.bindFirstLetter(item: CategoryWithType?) {
 @BindingAdapter("iconLetterBackground")
 fun TextView.bindBackgroundDrawable(item: CategoryWithType?) {
     item?.let {
+        val materialColors = context.resources.obtainTypedArray(R.array.material_colors)
+        val colorIndex = abs(it.category.id.hashCode()) % materialColors.length()
+        val color = materialColors.getColor(colorIndex, 0)
+
         val drawable = context.getDrawable(R.drawable.ic_shape_oval) as GradientDrawable
         drawable.mutate()
-        drawable.colors = MaterialColors.getRandomColor()
+        drawable.colors = intArrayOf(color, color)
 
         background = drawable
-    }
-}
-
-class MaterialColors {
-    companion object {
-        private val colors = listOf(
-                0xffe57373,
-                0xfff06292,
-                0xffba68c8,
-                0xff9575cd,
-                0xff7986cb,
-                0xff64b5f6,
-                0xff4fc3f7,
-                0xff4dd0e1,
-                0xff4db6ac,
-                0xff81c784,
-                0xffaed581,
-                0xffff8a65,
-                0xffd4e157,
-                0xffffd54f,
-                0xffffb74d,
-                0xffa1887f,
-                0xff90a4ae
-        )
-
-        fun getRandomColor(): IntArray {
-            val random = Random.nextInt(colors.size)
-            val color = colors[random].toInt()
-            val arr = IntArray(2)
-            arr[0] = color
-            arr[1] = color
-            return arr
-        }
     }
 }
